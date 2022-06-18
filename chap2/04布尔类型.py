d27a711fb06b27d25b6beac7b5277d2df91033c3
LeiGong2019 <1259361157@qq.com>
2022-06-18 23:04:07 +0800

    python

---
+++ chap2/04布尔类型.py
@@ -0,0 +1,10 @@
+# boolean， true表示1，真；false表示0，假
+
+f1=True
+f2=False
+print(f1,type(f1))
+print(f2,type(f2))
+
+# 布尔是表示1和0，也可以直接计算
+print(f1+1)
+print(f2+1)
---
+++ chap2/06数据类型转换.py
@@ -0,0 +1,8 @@
+# 在将不同数据类型的数据进行拼接的时候，就需要用到数据类型转换
+name='张三'
+age=20
+
+print(type(name),type(age))
+# name和age数据类型不一样
+
+print('我叫'+name+',今年'+str(age)+'岁')
