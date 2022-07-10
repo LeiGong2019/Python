1f9470fe8628ee5a63a92129f60a169ce0dba610
LeiGong2019 <58514635+LeiGong2019@users.noreply.github.com>
2022-07-11 00:34:36 +0800

    Delete chap2 directory

--- chap2/01变量.py
+++
@@ -1,8 +0,0 @@
-
-name='玛利亚'
-print(name)
-print('标识',id(name))
-# 内存地址
-print('类型',type(name))
-print('值',name)
-
--- chap2/07声明脚本编码.py
+++
@@ -1,4 +0,0 @@
-#coding:utf-8
-# 注意，此命令不是注释，而是声明脚本的编码；默认是utf-8
-
-print('hello world !')
