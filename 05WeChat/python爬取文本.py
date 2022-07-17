a62922223e413b881d8aedd9d7f252093dfbbf12
LeiGong2019 <1259361157@qq.com>
2022-07-17 18:37:21 +0800

    c

---
+++ 05WeChat/python爬取文本.py
@@ -0,0 +1,9 @@
+""" 
+    File Name:      爬取笔趣阁.py   笔趣阁：http://www.biquger.net/
+    Description:    爬虫
+    module:         
+    note:      
+
+    模块介绍：
+        requests库: python的第三方包,用来做接口测试,接口自动化测试,爬虫；       
+ """
--- 05WeChat/淘宝秒杀.py
+++ 05WeChat/淘宝秒杀.py
@@ -1,11 +1,14 @@
+""" 
+内容：      淘宝秒杀测试， 采用selenium 模块
+使用模块：  Selenium是一个用于web应用程序测试的工具， Selenium测试直接运行在浏览器中， 就像真正的用户在操作一样；
+额外安装：   pip install selenium     
+            pip install datetime
+            下载webdriver，下载网址： https://registry.npmmirror.com/binary.html?path=chromedriver/
+            
+                注意：安装完成后，Selenium需要配合webdriver才能使用，webdriver下载放置在任意非中文路径的文件夹下即可；
+                注意：选择一个和浏览器版本契合或最近的文件； 如chorme是101.0.4951.54，修改浏览器快捷方式中的目标，添加 --remote-debugging-port=6001
+"""
 
-
-# 淘宝秒杀测试
-# 模拟真人操作
-# Selenium是一个用于web应用程序测试的工具，Selenium测试直接运行在浏览器中，就像真正的用户在操作一样；
-# 安装 pip install selenium  ; 安装完成后，Selenium需要配合webdriver才能使用，webdriver下载放置在任意非中文路径的文件夹下即可；
-# 下载网址： https://registry.npmmirror.com/binary.html?path=chromedriver/,
-#  选择一个和浏览器版本契合或最近的文件； 如chorme是101.0.4951.54，修改浏览器快捷方式中的目标，添加 --remote-debugging-port=6001
 
 # 导入驱动
 from numpy import datetime_data
@@ -29,7 +32,7 @@ time.sleep(2)  # 等待2s，页面加载完成
 
 driver.find_element(By.ID,"J_SelectAll1").click()   # 选中ID属性点击，这里点击的是全选
 time.sleep(2)  # 等待2s，页面加载完成；  是因为自己电脑"结算"按键刷新太慢，要等待
-ti= '2022.7.13 21:50:00' #设置一个时间
+ti= '2022.7.17 01:16:00' #设置一个时间
 startTime = datetime.datetime.strptime(ti,'%Y.%m.%d %H:%M:%S')  #时间格式转化
 while datetime.datetime.now() < startTime:
     print(datetime.datetime.now())
